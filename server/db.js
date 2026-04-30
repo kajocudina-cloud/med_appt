bd02f7193df0c74687ec0865d35218ea5564419a
kajocudina-cloud <kajo.cudina@gmail.com>
2026-04-29 20:08:09 -0400

    Fix Sign_Up import path, update db connection and finalize Navbar logout

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:79wKTSEweGKLAZvxEpfLfA5C@172.21.124.115:27017";
+const mongoURI = "mongodb://127.0.0.1:27017/stayhealthy";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
